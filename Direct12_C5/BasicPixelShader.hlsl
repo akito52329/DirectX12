11b69859363502a83065bc10d4a9b4fc75d87704
akito52329 <sakuseimoto@gmail.com>
2022-08-25 11:10:34 +0900

    new

--- Direct12_C5/BasicPixelShader.hlsl
+++ Direct12_C5/BasicPixelShader.hlsl
@@ -1,6 +1,6 @@
 #include "BasicShaderHeader.hlsli"
 
-
-float4 BasicPS(Input input) : SV_TARGET{
-	return float4(input.uv, 1, 1);
+float4 BasicPS(Output input) : SV_TARGET{
+	//return float4(input.uv, 1, 1);
+	return float4(tex.Sample(smp, input.uv));
 }
